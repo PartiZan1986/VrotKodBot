c4006af30ceacf3f13ec78bd80c398a136e122dc
Yaroslav <kay3@yandex.ru>
2023-10-17 14:35:06 +0300

    Add project files.

---
+++ VrotKodBot/Controllers/TextMessageController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bots.Types;
+using System.Threading;
+
+
+namespace VrotKodBot.Controllers
+{
+    internal class TextMessageController
+    {
+        public readonly ITelegramBotClient _telegramClient;
+
+        public TextMessageController(ITelegramBotClient telegramBotClient)
+        {
+            _telegramClient = telegramBotClient;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            switch (message.Text)
+            {
+                case "/start":
+
+                    // Объект, представляющий кноки
+                    var buttons = new List<InlineKeyboardButton[]>();
+                    buttons.Add(new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData($" Подсчитать кол-во символов" , $"kolvo"),
+                        InlineKeyboardButton.WithCallbackData($" Вычеслить сумму" , $"sum")
+                    });
+
+
+                    if (callbackQuery.Data  == "kolvo" )
+                    {
+                        await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Длина сообщения: {message.Text.Length} знаков", cancellationToken: cancellationToken);
+                        return;
+                    }
+
+                    // передаем кнопки вместе с сообщением (параметр ReplyMarkup)
+                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"<b>  Наш бот превращает аудио в текст.</b> {Environment.NewLine}" +
+                        $"{Environment.NewLine}Можно записать сообщение и переслать другу, если лень печатать.{Environment.NewLine}", cancellationToken: ct, parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(buttons));
+
+                    break;
+                default:
+                    await _telegramClient.SendTextMessageAsync(message.Chat.Id, "Отправьте аудио для превращения в текст.", cancellationToken: ct);
+                    break;
+            }
+        }
+        //Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+        //await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено текстовое сообщение", cancellationToken: ct);
+    
+    }
+}
---
+++ VrotKodBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using VrotKodBot.Controllers;
+
+namespace VrotKodBot
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6302779481:AAHtTRzBOF4VpurScSce9h0UAnzB6kmwUMI"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddTransient<DefaultMessageController>();            
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+            services.AddHostedService<Bot>();
+        }
+    }
+}
